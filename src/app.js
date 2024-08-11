ef18a034a61b2f1393d3bc78020edbcf7cefe2b0
Rajni <balatamna16@gmail.com>
2024-08-11 10:07:12 -0400

    changes made

--- src/app.js
+++ src/app.js
@@ -22,14 +22,14 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
-    name: "Varun",
+    name: "Rajni",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
-    name: "Varun",
+    name: "Rajni",
   });
 });
 
@@ -54,14 +54,14 @@ app.get("/help", (req, res) => {
   res.render("help", {
     helpText: "This is some helpful text.",
     title: "Help",
-    name: "Varun",
+    name: "Rajni",
   });
 });
 
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Varun",
+    name: "Rajni",
     errorMessage: "Help article not found.",
   });
 });
@@ -69,7 +69,7 @@ app.get("/help/*", (req, res) => {
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Varun",
+    name: "Rajni",
     errorMessage: "Page not found.",
   });
 });
